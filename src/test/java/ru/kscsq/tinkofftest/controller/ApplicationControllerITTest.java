10fb0e858481018f3a18f2b1b374e0a1165ee691
kscsq <kscsq@mail.ru>
2022-07-20 17:52:40 +0300

    Dependencies versions up, tests fix

--- src/test/java/ru/kscsq/tinkofftest/controller/ApplicationControllerITTest.java
+++ src/test/java/ru/kscsq/tinkofftest/controller/ApplicationControllerITTest.java
@@ -1,15 +1,12 @@
 package ru.kscsq.tinkofftest.controller;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.kscsq.tinkofftest.TestData.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationControllerITTest {
 
