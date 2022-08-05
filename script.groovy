a5a86673c21b82f8c187a38a3da9c85661e03b3f
Fedor Dmitriev <kscsq@mail.ru>
2022-08-05 18:57:24 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,18 @@
-def buildApp() {
+def buildJar() {
   echo 'building the application...'
+  sh 'mvn package'
 }
 
-def testApp() {
-  echo 'testing the application...'
+def buildAndPushImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t kscsq/tinkofftesttask:jma-2.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push kscsq/tinkofftesttask:jma-2.0'
 }
 
 def deployApp() {
   echo 'deploying the application...'
-  echo "deploying version ${params.VERSION}"
 }
 
 return this
